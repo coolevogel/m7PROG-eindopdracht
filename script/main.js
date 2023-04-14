65ff6be7664ae396f70cb09e3ea4bf9c7dca79dd
mathtijs <matthijsjurriaans333@gmail.com>
2023-04-14 20:12:38 +0200

    footer en linker section uit javascript laten laden (left section moet alleen nog in een for loop)

--- script/main.js
+++ script/main.js
@@ -13,8 +13,86 @@ class Api {
     }
 }
 
+class Header {
+
+    // <header class="header">
+    //     <figure class="header__figure">
+    //         <img src="./images/logo.png" alt="foto van handsome squidward" class="header__img">
+    //     </figure>
+    //     <h1 class="header__title">Episodes</h1>
+    // </header>
+
+    placeToRenderHeader;
+    headerElement;
+    headerFigureElement;
+
+    constructor(placeToRenderHeader) {
+        this.placeToRenderHeader = document.getElementsByTagName(placeToRenderHeader)[0];
+
+        this.headerElement = document.createElement("header");
+        this.headerElement.classList = ("header");
+
+        this.headerFigureElement = document.createElement("figure");
+        this.headerFigureElement.classList = ("header__figure");
+
+        //voeg source aan img element toe
+        this.headerImgElement = document.createElement("img");
+        this.headerImgElement.classList = ("header__img");
+
+        this.headerH1Element = document.createElement("h1");
+        this.headerH1Element.classList = ("header__title");
 
 
+
+        this.render();
+    }
+
+    render() {
+        this.placeToRenderHeader.appendChild(this.headerElement);
+
+        this.headerElement.appendChild(this.headerFigureElement);
+        this.headerElement.appendChild(this.headerH1Element);
+
+        this.headerFigureElement.appendChild(this.headerImgElement);
+
+        this.renderContent();
+    }
+
+    renderContent() {
+        this.headerH1Element.innerHTML = "Episodes";
+    }
+}
+
+class Main {
+
+    // <main class="main">
+    //     <section class="midSection">
+    //     </section>
+    // </main>
+
+    placeToRenderMain;
+    mainElement;
+    midSectionElement
+
+    constructor(placeToRenderMain) {
+        this.placeToRenderMain = document.getElementsByTagName(placeToRenderMain)[0];
+
+        this.mainElement = document.createElement("main");
+        this.mainElement.classList = ("main");
+
+        this.midSectionElement = document.createElement("section");
+        this.midSectionElement.classList = ("midSection")
+
+        this.render();
+    }
+
+    render() {
+        this.placeToRenderMain.appendChild(this.mainElement);
+        this.mainElement.appendChild(this.midSectionElement);
+    }
+}
+
+class LeftSection {
 
     // <section class="section__left">
 
@@ -42,6 +120,126 @@ class Api {
     //     </ul>
 
     // </section>
+    placeToRenderLeftSection;
+    leftSectionElement;
+    listElement;
+    grouperElement1;
+    grouperElement2;
+
+    constructor(placeToRenderLeftSection){
+        this.placeToRenderLeftSection = placeToRenderLeftSection;
+
+        this.leftSectionElement = document.createElement("section");
+        this.leftSectionElement.classList = ("section__left");
+
+        this.listElement = document.createElement("ul");
+        this.listElement.classList = ("episodes");
+
+        this.grouperElement1 = document.createElement("div");
+        this.grouperElement1.classList = ("episodes__grouper");
+
+        this.grouperElement2 = document.createElement("div");
+        this.grouperElement2.classList = ("episodes__grouper");
+
+        // 1
+        this.listItemElement1 = document.createElement("li");
+        this.listItemElement1.classList = ("episodes__episode");
+
+        this.dateH3Element1 = document.createElement("h3");
+        this.dateH3Element1.classList = ("episodes__date");
+
+        this.titleH3Element1 = document.createElement("h3");
+        this.titleH3Element1.classList = ("episodes__title");
+
+        // 2
+        this.listItemElement2 = document.createElement("li");
+        this.listItemElement2.classList = ("episodes__episode");
+
+        this.dateH3Element2 = document.createElement("h3");
+        this.dateH3Element2.classList = ("episodes__date");
+
+        this.titleH3Element2 = document.createElement("h3");
+        this.titleH3Element2.classList = ("episodes__title");
+
+        // 3
+        this.listItemElement3 = document.createElement("li");
+        this.listItemElement3.classList = ("episodes__episode");
+
+        this.dateH3Element3 = document.createElement("h3");
+        this.dateH3Element3.classList = ("episodes__date");
+
+        this.titleH3Element3 = document.createElement("h3");
+        this.titleH3Element3.classList = ("episodes__title");
+
+        // 4
+        this.listItemElement4 = document.createElement("li");
+        this.listItemElement4.classList = ("episodes__episode");
+
+        this.dateH3Element4 = document.createElement("h3");
+        this.dateH3Element4.classList = ("episodes__date");
+
+        this.titleH3Element4 = document.createElement("h3");
+        this.titleH3Element4.classList = ("episodes__title");
+
+
+        this.render();
+    }
+
+    render(){
+        this.placeToRenderLeftSection.appendChild(this.leftSectionElement);
+
+        this.leftSectionElement.appendChild(this.listElement);
+        
+        this.listElement.appendChild(this.grouperElement1);
+        this.listElement.appendChild(this.grouperElement2);
+
+        // 1
+        this.grouperElement1.appendChild(this.listItemElement1);
+
+        this.listItemElement1.appendChild(this.dateH3Element1);
+        this.listItemElement1.appendChild(this.titleH3Element1);
+
+        // 2
+        this.grouperElement1.appendChild(this.listItemElement2);
+
+        this.listItemElement2.appendChild(this.dateH3Element2);
+        this.listItemElement2.appendChild(this.titleH3Element2);
+
+        // 3
+        this.grouperElement2.appendChild(this.listItemElement3);
+
+        this.listItemElement3.appendChild(this.dateH3Element3);
+        this.listItemElement3.appendChild(this.titleH3Element3);
+
+        // 4
+        this.grouperElement2.appendChild(this.listItemElement4);
+
+        this.listItemElement4.appendChild(this.dateH3Element4);
+        this.listItemElement4.appendChild(this.titleH3Element4);
+
+        
+        this.renderContent();
+    }
+
+    renderContent(){
+
+        // date
+        this.dateH3Element1.innerHTML = "datum";
+        this.dateH3Element2.innerHTML = "datum";
+        this.dateH3Element3.innerHTML = "datum";
+        this.dateH3Element4.innerHTML = "datum";
+
+        // title
+        this.titleH3Element1.innerHTML = "title";
+        this.titleH3Element2.innerHTML = "title";
+        this.titleH3Element3.innerHTML = "title";
+        this.titleH3Element4.innerHTML = "title";
+
+    }
+
+}
+
+class RightSection {
 
     // <section class="section__right">
     //     <div class="description__header">
@@ -82,105 +280,39 @@ class Api {
     // </section>
 
 
+}
+
+class Footer {
 
     //     <footer class="footer">
     //         <p class="footer__p">gemaakt door - Matthijs Jurriaans SD2C Media College Amsterdam</p>
     //     </footer>
 
-class Header {
+    placeToRenderFooter;
+    footerElement;
+    footerParagraphElement;
 
-    // <header class="header">
-    //     <figure class="header__figure">
-    //         <img src="./images/logo.png" alt="foto van handsome squidward" class="header__img">
-    //     </figure>
-    //     <h1 class="header__title">Episodes</h1>
-    // </header>
+    constructor(placeToRenderFooter){
+        this.placeToRenderFooter = placeToRenderFooter;
 
-    placeToRenderHeader;
-    headerElement;
-    headerFigureElement;
+        this.footerElement = document.createElement("footer");
+        this.footerElement.classList = ("footer");
 
-    constructor(placeToRenderHeader) {
-        this.placeToRenderHeader = document.getElementsByTagName(placeToRenderHeader)[0];
-
-        this.headerElement = document.createElement("header");
-        this.headerElement.classList = ("header");
-
-        this.headerFigureElement = document.createElement("figure");
-        this.headerFigureElement.classList = ("header__figure");
-
-        this.headerImgElement = document.createElement("img");
-        this.headerImgElement.classList = ("header__img");
-
-        this.headerH1Element = document.createElement("h1");
-        this.headerH1Element.classList = ("header__title");
-
-
+        this.footerParagraphElement = document.createElement("p");
+        this.footerParagraphElement.classList = ("footer__p")
 
         this.render();
     }
 
-    render() {
-        this.placeToRenderHeader.appendChild(this.headerElement);
-
-        this.headerElement.appendChild(this.headerFigureElement);
-        this.headerElement.appendChild(this.headerH1Element);
-
-        this.headerFigureElement.appendChild(this.headerImgElement);
-
+    render(){
+        this.placeToRenderFooter.appendChild(this.footerElement);
+        this.footerElement.appendChild(this.footerParagraphElement);
         this.renderContent();
     }
 
-    renderContent() {
-        this.headerH1Element.innerHTML = "Episodes";
+    renderContent(){
+        this.footerParagraphElement.innerHTML = "gemaakt door - Matthijs Jurriaans SD2C Media College Amsterdam";
     }
-}
-
-class Main {
-
-    /*<main class="main">
- 
-<section class="midSection">
- 
-</section>
- 
-</main> */
-
-    placeToRenderMain;
-    mainElement;
-    midSectionElement
-
-    constructor(placeToRenderMain) {
-        this.placeToRenderMain = document.getElementsByTagName(placeToRenderMain)[0];
-
-        this.mainElement = document.createElement("main");
-        this.mainElement.classList = ("Main");
-
-        this.midSectionElement = document.createElement("section");
-        this.midSectionElement.classList = ("midSection")
-
-        this.render();
-    }
-
-    render() {
-        this.placeToRenderMain.appendChild(this.mainElement);
-        this.mainElement.appendChild(this.midSectionElement);
-    }
-}
-
-class leftSection {
-
-}
-
-class rightSection {
-
-}
-
-class DetailCard {
-
-}
-
-class Footer {
 
 }
 
@@ -191,11 +323,17 @@ class App {
     api;
     header;
     main;
+    leftSection;
+    rightSection;
+    footer;
 
     constructor() {
         this.api = new Api();
         this.header = new Header("body");
         this.main = new Main("body");
+        this.leftSection = new LeftSection(this.main.midSectionElement);
+        this.rightSection = new RightSection(this.main.midSectionElement);
+        this.footer = new Footer(this.main.mainElement);
 
         this.api.GetData().then(
             () => {
